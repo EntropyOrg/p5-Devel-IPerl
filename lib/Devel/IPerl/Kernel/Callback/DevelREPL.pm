7e1b5b34cc42e8cdac95d7880f5ca881679f2874
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-06-19 19:53:02 -0500

    wip: output using pyout msg_type

--- lib/Devel/IPerl/Kernel/Callback/DevelREPL.pm
+++ lib/Devel/IPerl/Kernel/Callback/DevelREPL.pm
@@ -2,10 +2,11 @@ package Devel::IPerl::Kernel::Callback::DevelREPL;
 
 use strict;
 use Moo;
+use UUID::Tiny ':std';
 
 extends qw(Devel::IPerl::Kernel::Callback);
 
-has execution_counter => ( is => 'rw' );
+has execution_count => ( is => 'rw', default => sub { 0 } );
 
 sub msg_execute_request {
 	my ($self, $kernel, $blobs, $msg ) = @_;
@@ -39,11 +40,32 @@ sub msg_execute_request {
 		content => {
 			status => 'ok',
 			execution_count => $self->execution_count,
+			payload => [],
+			user_variables => {},
+			user_expressions => {},
 		}
 	);
 	$kernel->send_message( $kernel->shell, $execute_reply, $uuid );
 
 	# TODO send display_data / pyout
+	my $output = Devel::IPerl::Kernel::Message->new(
+		header => {
+			msg_type => 'pyout', # this changes in v5.0 of protocol
+			msg_id => create_uuid_as_string(),
+			session => $msg->session,
+			username => $msg->username,
+		},
+		parent_header => $msg->header,
+		content => {
+			execution_count => $self->execution_count,
+			data => {
+				'text/plain' => 'test',
+			},
+			metadata => {},
+		}
+	);
+	$kernel->send_message( $kernel->iopub, $output, $uuid );
+
 
 	# TODO send kernel status : idle
 	my $status_idle = Devel::IPerl::Kernel::Message->new(
