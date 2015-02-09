a0a2aad44d2c640525754971f630119403025808
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-02-09 12:23:24 -0600

    add IPerl package
    This is used to hold all plugin helpers

---
+++ lib/IPerl.pm
@@ -0,0 +1,45 @@
+package IPerl;
+
+use strict;
+use warnings;
+use Moo;
+with qw( MooX::Singleton );
+
+our $REPL;
+our $current_msg;
+
+has helpers => ( is => 'rw', default => sub { +{} } );
+
+=method helper
+
+    IPerl->helper( $name, $coderef )
+
+Register helper callback.
+
+=cut
+sub helper {
+	my ($class, $name, $cb) = @_;
+	my $self = $class->instance;
+	warn (qq{Helper "$name" already exists, replacing.})
+		if exists $self->helpers->{$name};
+	$self->add_helper($name => $cb);
+}
+
+sub add_helper {
+	my ($self, $name, $cb) = @_;
+	$self->helpers->{$name} = $cb;
+	{
+		no strict 'refs';
+		no warnings 'redefine';
+		*{"IPerl::$name"} = $cb;
+	}
+}
+
+sub load_plugin {
+	my ($class, $plugin_name) = @_;
+	my $plugin_package = "Devel::IPerl::Plugin::$plugin_name";
+	{ no strict; eval "require $plugin_package"; }
+	$plugin_package->register( $class );
+}
+
+1;
