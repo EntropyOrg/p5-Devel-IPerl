cea593ebd4fa31867da76b825d7a5d47e8e10f9d
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-05-02 20:07:57 -0500

    remove the IPython profile tempate in share/

--- share/profile/ipython_config.py
+++
@@ -1,9 +0,0 @@
-# IPerl VERSION
-c = get_config()
-#c.KernelManager.kernel_cmd = ['perl', '-MDevel::IPerl', '-e1', 'kernel', '{connection_file}']
-c.KernelManager.kernel_spec = 'iperl'
-c.Session.key = b''
-c.Session.keyfile = b''
-
-# Syntax highlight for Perl notebooks.
-c.NbConvertBase.default_language = "perl"
