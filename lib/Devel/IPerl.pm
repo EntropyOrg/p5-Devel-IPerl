022b24bde2e7ccca509edfdbec17f706138ec23e
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-05-21 22:31:16 -0500

    add `nbconvert.iperl` command to the bin/iperl script

--- lib/Devel/IPerl.pm
+++ lib/Devel/IPerl.pm
@@ -5,14 +5,19 @@ use strict;
 use warnings;
 
 use Devel::IPerl::Kernel;
+use Devel::IPerl::NBConvert;
 
 sub main {
 	if ( @ARGV >= 1 ) {
 		if( @ARGV >= 2 and $ARGV[0] eq 'kernel' ) {
 			my $kernel = Devel::IPerl::Kernel->new( connection_file => $ARGV[1] );
 			$kernel->run;
+		} elsif( $ARGV[0] eq 'nbconvert.iperl' ) {
+			shift @ARGV;
+			Devel::IPerl::NBConvert->new()->run;
 		}
 	}
+	return 0;
 }
 
 1;
